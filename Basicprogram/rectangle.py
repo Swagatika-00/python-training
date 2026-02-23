c18869ec690b55e7b65885f75c34e086aeba818d
Swagatika Jena <sjswagatika2003@gmail.com>
2026-02-23 10:28:52 +0530

    test

---
+++ Basicprogram/rectangle.py
@@ -0,0 +1,27 @@
+"""
+Wap take rectangle length and breadth from keyboard
+display lenghth area and perimeter
+"""
+print("enter rectangle lenghth")
+L=int(input())
+print("enter rectangle breadth")
+B=int(input())
+ar=L*B
+pr=2*(L+B)
+print("lenghth=",L)
+print("breadth=",B)
+print("area=",ar)
+print("perimeter=",pr)
+
+"""
+o/p:
+enter rectangle lenghth
+3
+enter rectangle breadth
+4
+lenghth= 3
+breadth= 4
+area= 12
+perimeter= 14
+
+"""
