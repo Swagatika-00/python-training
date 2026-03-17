4e78dfe8af7352d89eea3a94c2613e1217ad2e01
Swagatika Jena <sjswagatika2003@gmail.com>
2026-03-17 13:11:25 +0530

    overloading

---
+++ Operator overloading/oper3.py
@@ -0,0 +1,7 @@
+class MyTime:
+	def __init__(self,h,m,s):
+		self:h=m 
+		self:m=m 
+		self.s=s 
+	def __it__(self,tt2)	
+	
---
+++ Operator overloading/opr1.py
@@ -0,0 +1,18 @@
+class Mycomplex:
+	def __init__(self,r,i)
+		self.r=r
+	    self.i=i
+	def __add__(self.c2):
+	    c3=Mycomplex(0,0)
+	    c3.r=self.r+c2.try:
+	    	pass
+	    except Exception as e:
+	    	raise
+	    else:
+	    	pass
+	    finally:
+	    	pass    
+		
+		
+	
+	
